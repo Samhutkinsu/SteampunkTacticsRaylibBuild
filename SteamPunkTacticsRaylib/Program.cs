858aae16671fe3dee44fff285f4240336855c119
Samhutkinsu <117354925+Samhutkinsu@users.noreply.github.com>
2023-12-12 09:05:44 -0500

    Initial Commit 12/12/23

---
+++ SteamPunkTacticsRaylib/Program.cs
@@ -0,0 +1,19 @@
+﻿using Raylib_cs;
+namespace SteamPunkTacticsRaylib
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+           
+
+            while(!Raylib.WindowShouldClose())
+            {
+                
+            }
+            Raylib.CloseWindow();
+        }
+    }
+}
